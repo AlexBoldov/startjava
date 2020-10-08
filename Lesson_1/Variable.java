bedc9111420ec819529e25deba8f02b206b7ecdb
Alex Boldov <chipoid.ru@gmail.com>
2020-10-08 11:34:42 +0300

    HW_01, удалена пустая строка перед boolean isIntegratedVideo = true; убраны избыточные комментарии

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,16 +1,13 @@
 public class Variable {
 	public static void main(String[] args) {
-		byte numberOfCores = 8;				//Число ядер процессора
-		short bitness = 64;					//Разрядность ОС
-		int memorySize = 16344;				//Объем ОЗУ, Mb
-		long hddSize = 1048576;				//Объем винчестера, Mb
-		float osVersion = 7.1f;				//Версия ОС
-		double cpuFrequency = 3.33;			//Частота процессора, GHz
-		char procMarking = 'T';				/*Маркировка процессора:
-											"T" - уменьшенное энергопотребление;
-											"K" - потенциал для разгона*/
-		boolean isIntegratedVideo = true;	//Наличие интегрированной видеокарты
-
+		byte numberOfCores = 8;
+		short bitness = 64;
+		int memorySize = 16344;
+		long hddSize = 1048576;
+		float osVersion = 7.1f;
+		double cpuFrequency = 3.33;
+		char procMarking = 'T';
+		boolean isIntegratedVideo = true;
 		System.out.println(numberOfCores);
 		System.out.println(bitness);
 		System.out.println(memorySize);
