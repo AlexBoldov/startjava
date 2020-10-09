638c02683f309bb41dea2ca280ede5278b1ffef2
Alex Boldov <chipoid.ru@gmail.com>
2020-10-09 14:03:30 +0300

    HW_2 выполнен Person

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+class Person {
+
+	String name = "Alex";
+	String gender = "male";
+	int age = 37;
+	int height = 181;
+	int weight = 76;
+
+	void move() {
+
+	}
+
+	void sit() {
+
+	}
+
+	void run() {
+
+	}
+
+	String speak() {
+
+	}
+
+	String learnJava() {
+
+	}
+}
