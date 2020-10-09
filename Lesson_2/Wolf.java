9e69bbfa5316452193255c4040bc17e49cb94b3a
Alex Boldov <chipoid.ru@gmail.com>
2020-10-09 17:06:47 +0300

    HW_2 выполнен п.5

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,28 +1,72 @@
-class Wolf {
+public class Wolf {
 
-	String name;
-	String gender;
-	int age;
-	int weight;
-	String color;
+	private String name;
+	private String gender;
+	private int age;
+	private int weight;
+	private String color;
 
-	void move() {
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		if (age > 8) {
+			System.out.println("Некорректный возраст");
+		} else {
+			this.age = age;
+		}
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public void move() {
 		System.out.println("Волк передвигается");
 	}
 
-	void sit() {
+	public void sit() {
 		System.out.println("Волк садится");
 	}
 
-	void run() {
+	public void run() {
 		System.out.println("Волк бежит");
 	}
 
-	String howl() {
+	public String howl() {
 		return "Волк воет";
 	}
 
-	String hunt() {
+	public String hunt() {
 		return "Волк охотится";
 	}
 }
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -2,17 +2,17 @@ public class WolfTest {
 
 	public static void main(String[] args) {
 		Wolf mowgliMother = new Wolf();
-		mowgliMother.name = "Ракша";
-		mowgliMother.gender = "female";
-		mowgliMother.age = 15;
-		mowgliMother.weight = 23;
-		mowgliMother.color = "white";
+		mowgliMother.setName("Ракша");
+		mowgliMother.setGender("female");
+		mowgliMother.setAge(9);
+		mowgliMother.setWeight(23);
+		mowgliMother.setColor("white");
 
-		System.out.println("Имя волчицы-матери Маугли: " + mowgliMother.name);
-		System.out.println("Пол волчицы-матери Маугли: " + mowgliMother.gender);
-		System.out.println("Возраст волчицы-матери Маугли: " + mowgliMother.age);
-		System.out.println("Вес волчицы-матери Маугли: " + mowgliMother.weight);
-		System.out.println("Окраска волчицы-матери Маугли: " + mowgliMother.color);
+		System.out.println("Имя волчицы-матери Маугли: " + mowgliMother.getName());
+		System.out.println("Пол волчицы-матери Маугли: " + mowgliMother.getGender());
+		System.out.println("Возраст волчицы-матери Маугли: " + mowgliMother.getAge());
+		System.out.println("Вес волчицы-матери Маугли: " + mowgliMother.getWeight());
+		System.out.println("Окраска волчицы-матери Маугли: " + mowgliMother.getColor());
 
 		mowgliMother.move();
 		mowgliMother.sit();
