2e8f0e5c05bb1c95c1bb6ee822f42f5df1fcfc6b
Alex Boldov <chipoid.ru@gmail.com>
2020-10-19 00:18:23 +0300

    HW_5 psql

---
+++ src/com/startjava/lesson_5/init_db.sql
@@ -0,0 +1,14 @@
+-- init database
+
+INSERT INTO Jaegers(modelName, mark, height, weight, status, origin, launch, kaijuKill)
+	VALUES
+		('Horizon Brave',     'Mark-1', '72.54', '7890', 'Destroyed', 'China',     'December 22, 2015', '2'),
+		('Romeo Blue',	      'Mark-1', '77.72', '7775', 'Destroyed', 'USA',       'December 15, 2015', '2'),
+		('Gipsy Danger',      'Mark-3', '79.25', '1980', 'Destroyed', 'USA',       'July 10, 2017',     '9'),
+		('Crimson Typhoon',   'Mark-4', '76.2',  '1722', 'Destroyed', 'China',     'August 22, 2018',   '7'),
+		('Striker Eureka',    'Mark-5', '76.2',  '1850', 'Destroyed', 'Australia', 'November 2, 2019',  '11'),
+		('Gipsy Avenger',     'Mark-6', '81.77', '2004', 'Destroyed', 'Australia', 'January 12, 2034',  '1'),
+		('Guardian Bravo',    'Mark-6', '73.21', '1975', 'Active',    'Australia', 'December 15, 2033', '0'),
+		('Saber Athena',      'Mark-7', '76.91', '1628', 'Destroyed', 'China',     'December 20, 2033', '1'),
+		('Chronos Berserker', 'Mark-5', '75.42', '7890', 'Active',    'Australia', 'December 1, 2018',  '4'),
+		('Russian Bear',      'Mark-1', '99.99', '9999', 'Active',    'Russia',    'April 1, 2015',     '25');
